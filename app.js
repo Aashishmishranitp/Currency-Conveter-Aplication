6253433c8753c4dc1ea5810cae74d57a9a9dd8d3
Aashish Mishra <aashishm.dd22.cs@nitp.ac.in>
2024-04-16 23:55:32 +0530

    add api url changes modification

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-
+"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
@@ -25,21 +24,48 @@ for (let select of dropdowns) {
   });
 }
 
-const updateExchangeRate = async () => {
-  let amount = document.querySelector(".amount input");
-  let amtVal = amount.value;
-  if (amtVal === "" || amtVal < 1) {
-    amtVal = 1;
-    amount.value = "1";
-  }
-  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-  let response = await fetch(URL);
-  let data = await response.json();
-  let rate = data[toCurr.value.toLowerCase()];
+// const updateExchangeRate = async () => {
+//   let amount = document.querySelector(".amount input");
+//   let amtVal = amount.value;
+//   if (amtVal === "" || amtVal < 1) {
+//     amtVal = 1;
+//     amount.value = "1";
+//   }
+//   const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+//   let response = await fetch(URL);
+//   let data = await response.json();
+//   let rate = data[toCurr.value.toLowerCase()];
 
-  let finalAmount = amtVal * rate;
-  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
-};
+//   let finalAmount = amtVal * rate;
+//   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+// };
+
+const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+
+let toCurrval = toCurr.value.toLowerCase();
+console.log(toCurrval)
+const fromCurrVal = fromCurr.value.toLowerCase();
+console.log(fromCurrVal)
+async function rares(){
+  try {
+    const response = await fetch(URL);
+    // console.log(response);
+    const data= await response.json();
+    // console.log(data)
+    let rate = data[fromCurrVal][toCurrval];
+    // console.log(rate)
+    let amount = document.querySelector("#inputA").value;
+    console.log(amount)
+    const finalAmount = amount*rate;
+    msg.innerText = `${amount} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    
+    
+    
+  } catch (error) {
+    console.log("E ",error)
+  }
+ }
+
 
 const updateFlag = (element) => {
   let currCode = element.value;
@@ -51,9 +77,9 @@ const updateFlag = (element) => {
 
 btn.addEventListener("click", (evt) => {
   evt.preventDefault();
-  updateExchangeRate();
+  rares();
 });
 
-window.addEventListener("load", () => {
-  updateExchangeRate();
-});
+// window.addEventListener("load", () => {
+//   updateExchangeRate();
+// });
